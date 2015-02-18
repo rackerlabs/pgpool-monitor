2eb73b6871362564cd492d7a0a3060eded3656fc
Alex Schultz <alex.schultz@rackspace.com>
2015-02-18 11:14:41 -0600

    improving error handling

--- pgpool_monitor/__init__.py
+++ pgpool_monitor/__init__.py
@@ -14,13 +14,29 @@ def run():
         'pgpool_monitor.cfg',
         os.path.expanduser('~/.pgpool_monitor.cfg'),
         ])
-    st = status.Status(config=config)
-    r = st.get_stat(sys.argv[1])
-    if r is not False:
-        print r
-    else:  # 0 for failure
-        print 0
-    sys.exit(0)
+    return_code = 0
+    if len(sys.argv) < 2:
+        print """Usage: pgpool_monitor <stat>
+        Valid stats:
+        - READ
+        - WRITE
+        - NODE_COUNT
+        - ACTIVE_COUNT
+        - NUM_PROCS
+        """
+        sys.exit(1)
+
+    try:
+        st = status.Status(config=config)
+        r = st.get_stat(sys.argv[1])
+        if r is not False:
+            print r
+        else:  # 0 for failure
+            print 0
+    except Exception as e:
+        print "Error, {s}".format(s=e)
+        return_code = 1
+    sys.exit(return_code)
 
 if __name__ == '__main__':
     run()
